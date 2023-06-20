c30304feb6d0f8db2ccdd36bac7cfe4f3fe62cd3
jonimarqu <marquez.jonas91@gmail.com>
2023-06-20 17:57:49 -0300

    Config DB with sequelize + sqlite3 +Fix bug in 'start:dev' script in package.json

---
+++ config/db-config.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require("sequelize"); // take the Sequelize Class
+
+// make a unique instance of the class
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./database.sqlite",
+});
+
+const initializeDB = async () => {
+  try {
+    await sequelize.authenticate(); // Test the connection by trying to authenticate
+    console.log("Conextion to DB stablished.");
+
+    // Sync all defined models to the DB
+    await sequelize.sync({ force: true }); // force: if true, each start deletes DB
+  } catch (err) {
+    console.error("Error initializing DB.");
+  }
+};
+
+module.exports = { sequelize, initializeDB };
